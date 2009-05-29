1245129b0bf3e29c14291be7fee0ae03be0d66b4
alastairs <github@alastairsmith.me.uk>
2009-05-29 23:43:02 +0100

    Adding parallel implementation of quick sort and removing Gui program as it's non-trivial to wire it up.

--- ParallelQuickSort/ParallelQuickSort.cs
+++ ParallelQuickSort/ParallelQuickSort.cs
@@ -14,24 +14,50 @@ namespace ParallelQuickSort
             set;
         }
 
+        /// <summary>
+        /// Sorts the array of numbers using a parallel quick sort implementation.
+        /// </summary>
         public void Sort()
         {
             QuickSort(0, Numbers.Count - 1);
         }
 
+        /// <summary>
+        /// Actually does the sorting
+        /// </summary>
+        /// <param name="left">The lower bound index for the array segment</param>
+        /// <param name="right">The upper bound index for the array segment</param>
+        /// <returns>The list of numbers, sorted</returns>
         private IList<int> QuickSort(int left, int right)
         {
             if (right > left)
             {
                 int pivot = left;
                 int newPivot = Partition(left, right, pivot);
-                QuickSort(left, newPivot - 1).AsParallel();
-                QuickSort(newPivot + 1, right).AsParallel();
+
+                // The two recursive calls to QuickSort work on separate sections
+                // of the array of numbers, so these can be invoked in parallel
+                Parallel.Invoke(delegate
+                {
+                    QuickSort(left, newPivot - 1).AsParallel();
+                }, 
+
+                delegate
+                {
+                    QuickSort(newPivot + 1, right).AsParallel();
+                });
             }
 
             return Numbers;
         }
 
+        /// <summary>
+        /// Helper method to calculate the partition in the array of numbers.
+        /// </summary>
+        /// <param name="left">The lower bound index for the array segment</param>
+        /// <param name="right">The upper bound index for the array segment</param>
+        /// <param name="pivotIndex">The current index of the pivot</param>
+        /// <returns>The new index of the pivot</returns>
         private int Partition(int left, int right, int pivotIndex)
         {
             int pivotValue = Numbers[pivotIndex];
